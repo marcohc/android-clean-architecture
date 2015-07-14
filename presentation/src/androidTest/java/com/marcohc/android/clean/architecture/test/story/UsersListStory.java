d5bbb812e030e4baa3b79d529519122106508d94
Marco Hernaiz Cao <m.hernaizcao@xs2theworld.com>
2015-07-14 13:29:37 +0200

    Testing WIP XX

--- presentation/src/androidTest/java/com/marcohc/android/clean/architecture/test/story/UsersListStory.java
+++ presentation/src/androidTest/java/com/marcohc/android/clean/architecture/test/story/UsersListStory.java
@@ -33,6 +33,7 @@ public class UsersListStory {
     @Test
     public void testTheListIsLoaded() {
 
+        // ASDASD ASD ASD
         showMessage("testTheListIsLoaded");
 
         Given.theUserIsNotLoggedIn();
