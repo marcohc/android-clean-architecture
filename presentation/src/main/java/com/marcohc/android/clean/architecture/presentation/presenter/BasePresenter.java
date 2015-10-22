59a6b4a59c1e74526be11c119d026ccd5c109151
Marco Hernaiz Cao <m.hernaizcao@xs2theworld.com>
2015-10-22 10:42:34 +0200

    Updating...

--- presentation/src/main/java/com/marcohc/android/clean/architecture/presentation/presenter/BasePresenter.java
+++ presentation/src/main/java/com/marcohc/android/clean/architecture/presentation/presenter/BasePresenter.java
@@ -3,44 +3,116 @@ package com.marcohc.android.clean.architecture.presentation.presenter;
 import android.util.Log;
 
 import com.hannesdorfmann.mosby.mvp.MvpBasePresenter;
-import com.marcohc.android.clean.architecture.common.exception.DataException;
 import com.marcohc.android.clean.architecture.common.util.Constants;
+import com.marcohc.android.clean.architecture.presentation.R;
 import com.marcohc.android.clean.architecture.presentation.view.inter.BaseView;
 
+@SuppressWarnings("ConstantConditions")
 public abstract class BasePresenter<V extends BaseView> extends MvpBasePresenter<V> {
 
     /**
-     * Shows or hides loading dialog
-     *
-     * @param hasToShow
+     * Shows or hides dialog with "Loading..." text by default
      */
-    public void showLoading(boolean hasToShow) {
-        if (getView() != null) {
-            getView().showLoading(hasToShow);
+    public void showLoadingDialog() {
+        if (isViewAttached()) {
+            getView().showLoadingDialog();
+        }
+    }
+
+    /**
+     * Shows dialog with message
+     *
+     * @param message dialog message
+     */
+    public void showDialog(String message) {
+        if (isViewAttached()) {
+            getView().showDialog(message);
+        }
+    }
+
+    /**
+     * Shows dialog with message
+     *
+     * @param message dialog message
+     */
+    public void showDialog(String title, String message) {
+        if (isViewAttached()) {
+            getView().showDialog(title, message);
+        }
+    }
+
+    /**
+     * Shows dialog with message
+     *
+     * @param message dialog message
+     */
+    public void showDialog(String title, String message, boolean isCancelable) {
+        if (isViewAttached()) {
+            getView().showDialog(title, message, isCancelable);
+        }
+    }
+
+    /**
+     * Shows or hides dialog with "Loading..." text by default
+     */
+    public void hideDialog() {
+        if (isViewAttached()) {
+            getView().hideDialog();
         }
     }
 
     /**
      * Shows error message
      *
-     * @param errorMessage
+     * @param errorMessage the error message to be shown
      */
     public void showError(String errorMessage) {
-        getView().showError(errorMessage);
+        if (isViewAttached()) {
+            getView().showError(errorMessage);
+        }
+    }
+
+    /**
+     * Shows warning message
+     *
+     * @param warningMessage the error message to be shown
+     */
+    public void showWarning(String warningMessage) {
+        if (isViewAttached()) {
+            getView().showWarning(warningMessage);
+        }
+    }
+
+    /**
+     * Shows message
+     *
+     * @param message the message to be shown
+     */
+    public void showInfo(String message) {
+        if (isViewAttached()) {
+            getView().showInfo(message);
+        }
     }
 
     /**
      * Handles the error showing a fancy Toasteroid and logging the error
      *
-     * @param error
+     * @param error the data exception error
      */
-    public void handleException(DataException error) {
-        getView().showLoading(false);
-        getView().showError(error.getMessage());
+    public void handleException(DataError error) {
+        hideDialog();
+        if (isViewAttached()) {
+            if (error.getCode() == FirebaseError.NETWORK_ERROR) {
+                showError(getView().getResourceString(R.string.internet_error));
+            } else {
+                showError(error.getMessage());
+            }
+        }
+
         Log.e(Constants.LOG_TAG, "Exception: " + error.getMessage());
     }
 
-    public void onEventMainThread(DataException exception) {
+    public void onEventMainThread(DataError exception) {
         handleException(exception);
     }
 }
--- presentation/src/main/java/com/marcohc/android/clean/architecture/presentation/view/inter/BaseView.java
+++ presentation/src/main/java/com/marcohc/android/clean/architecture/presentation/view/inter/BaseView.java
@@ -4,10 +4,33 @@ import com.hannesdorfmann.mosby.mvp.MvpView;
 
 public interface BaseView extends MvpView {
 
-    void showLoading(boolean show);
+    // Dialogs
+
+    void showLoadingDialog();
+
+    void showLoadingDialog(boolean isCancelable);
+
+    void showDialog(String message);
+
+    void showDialog(String message, boolean isCancelable);
+
+    void showDialog(String title, String message);
+
+    void showDialog(String title, String message, boolean isCancelable);
+
+    void hideDialog();
+
+    // Toast
+
+    void showInfo(String message);
+
+    void showWarning(String error);
 
     void showError(String error);
 
+    // Messages from string values
+
     String getResourceString(int stringId);
 
+    String getResourceString(int stringId, Object... formatArgs);
 }
