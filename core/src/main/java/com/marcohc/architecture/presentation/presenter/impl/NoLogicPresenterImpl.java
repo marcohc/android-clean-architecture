1a20ddd7d46f4a7c5f0fe46119291bec1d4a46fe
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2016-04-04 12:52:35 +0200

    Ignore domain errors in NoLogicPresenter

--- core/src/main/java/com/marcohc/architecture/presentation/presenter/impl/NoLogicPresenterImpl.java
+++ core/src/main/java/com/marcohc/architecture/presentation/presenter/impl/NoLogicPresenterImpl.java
@@ -1,8 +1,13 @@
 package com.marcohc.architecture.presentation.presenter.impl;
 
+import com.marcohc.architecture.domain.error.DomainError;
 import com.marcohc.architecture.presentation.presenter.BasePresenter;
 import com.marcohc.architecture.presentation.presenter.inter.NoLogicPresenter;
 import com.marcohc.architecture.presentation.view.inter.NoLogicView;
 
 public class NoLogicPresenterImpl extends BasePresenter<NoLogicView> implements NoLogicPresenter {
+    @Override
+    public void handleException(DomainError error) {
+        // Ignore
+    }
 }
