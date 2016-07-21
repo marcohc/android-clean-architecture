b5701d16dc82c932e1e909c44416eaaa4cfa0130
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2016-07-21 15:49:41 +0200

    Refactor DateHelper methods

--- core/src/main/java/com/marcohc/architecture/common/helper/DateHelper.java
+++ core/src/main/java/com/marcohc/architecture/common/helper/DateHelper.java
@@ -28,7 +28,7 @@ public class DateHelper {
     // * Calendar methods
     // ************************************************************************************************************************************************************************
 
-    public static String getCalendarString(Calendar calendar) {
+    public static String formatCurrentMonth(Calendar calendar) {
         return FormatterHelper.getFirstCapitalize(String.format(Locale.getDefault(), "%tB", calendar));
     }
 
@@ -153,7 +153,6 @@ public class DateHelper {
         calendar.set(Calendar.MINUTE, 0);
         calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
-        calendar.add(Calendar.SECOND, -1);
         return calendar;
     }
 
@@ -161,6 +160,7 @@ public class DateHelper {
         calendar.set(Calendar.HOUR_OF_DAY, 23);
         calendar.set(Calendar.MINUTE, 59);
         calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
         return calendar;
     }
 
