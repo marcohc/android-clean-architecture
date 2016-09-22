8278ab504aa08b052fb96f48322a20550dcf589a
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2016-09-22 15:57:13 +0200

    Fixed EventBus subscription in Views

--- core/src/main/java/com/marcohc/architecture/presentation/view/activity/BaseMvpActivity.java
+++ core/src/main/java/com/marcohc/architecture/presentation/view/activity/BaseMvpActivity.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 
 import com.hannesdorfmann.mosby.mvp.MvpActivity;
 import com.hannesdorfmann.mosby.mvp.MvpPresenter;
+import com.marcohc.architecture.presentation.bus.BusProvider;
 import com.marcohc.architecture.presentation.view.BaseMvpView;
 
 import butterknife.ButterKnife;
@@ -49,6 +50,18 @@ public abstract class BaseMvpActivity<V extends BaseMvpView, P extends MvpPresen
      */
     @LayoutRes
     protected abstract int getLayoutId();
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        BusProvider.register(presenter);
+    }
+
+    @Override
+    public void onStop() {
+        BusProvider.unregister(presenter);
+        super.onStop();
+    }
 
     @CallSuper
     @Override
