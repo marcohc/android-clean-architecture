7a3e68ebe2d9e43de464deeed68d13bc8db6c5e4
Marco Hernaiz Cao <marcohernaizcao@gmail.com>
2016-10-08 13:39:12 +0200

    Fixed subscription to DomainException in BaseBusMvpPresenter

--- bus/src/main/java/com/marcohc/architecture/presentation/bus/presentation/mvp/BaseBusMvpPresenter.java
+++ bus/src/main/java/com/marcohc/architecture/presentation/bus/presentation/mvp/BaseBusMvpPresenter.java
@@ -3,10 +3,14 @@ package com.marcohc.architecture.presentation.bus.presentation.mvp;
 import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 
+import com.marcohc.architecture.domain.exception.DomainException;
 import com.marcohc.architecture.presentation.bus.domain.executor.BusJobExecutor;
 import com.marcohc.architecture.presentation.bus.domain.interactor.BusUseCase;
 import com.marcohc.architecture.presentation.presenter.BaseMvpPresenter;
 import com.marcohc.architecture.presentation.view.BaseMvpView;
+
+import org.greenrobot.eventbus.Subscribe;
+import org.greenrobot.eventbus.ThreadMode;
 
 import timber.log.Timber;
 
@@ -23,6 +27,11 @@ public abstract class BaseBusMvpPresenter<V extends BaseMvpView> extends BaseMvp
     protected void executeUseCase(@NonNull BusUseCase useCase) {
         Timber.d("executeUseCase: %s", useCase.getClass().getSimpleName());
         BusJobExecutor.getInstance().executeHighPriorityUseCase(useCase);
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    public void onDomainException(DomainException domainException) {
+        handleException(domainException);
     }
 
 }
